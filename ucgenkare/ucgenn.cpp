d0767dac59206db7c70f13c1505dfcccdb29074a
esraaytac <109952743+esraaytac@users.noreply.github.com>
2023-02-03 19:39:14 +0300

    Add files via upload
    üçgen ve kare oluşturan fonksiyon

---
+++ ucgenkare/ucgenn.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void ucgen () {
+	
+	printf(" /\\ \n");
+	printf("/  \\  \n");
+	printf("---- \n");
+	printf("bu bir ucgen \n \n");
+}
+void kare () {
+	
+	printf("----- \n");
+	printf("|   | \n");
+	printf("|   | \n");
+	printf("----- \n");
+	printf("bu bir kare");
+}
+	
+int main (){
+
+ucgen();
+kare();
+
+
+	return 0;
+}	
